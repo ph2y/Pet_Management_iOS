1728faa56ac7e6f380c4b5ae9ba49e3e9e084b3b
newcentury99 <smyun99@gmail.com>
2022-02-25 00:03:50 +0900

    Implement PetSchedule reminder push notification logic

--- pet_management/src/ui/myschedule/UIMyPetScheduleVC.swift
+++ pet_management/src/ui/myschedule/UIMyPetScheduleVC.swift
@@ -8,7 +8,11 @@
 import UIKit;
 import Alamofire;
 
-class UIMyPetScheduleVC: UIViewController {
+protocol UIMyPetScheduleDelegate {
+    func scheduleStateChanged();
+}
+
+class UIMyPetScheduleVC: UIViewController, UIMyPetScheduleDelegate {
     @IBOutlet weak var scheduleListTableView: UITableView!;
     var scheduleList: [PetSchedule] = [];
     
@@ -19,6 +23,93 @@ class UIMyPetScheduleVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         self.reqHttpFetchPetSchedule();
+    }
+    
+    // func scheduleStateChanged
+    // No Param
+    // Return Void
+    // Call syncScheduleNotification when schedule enable switch is tapped
+    func scheduleStateChanged() {
+        self.syncScheduleNotification();
+    }
+    
+    // func syncScheduleNotification
+    // No Param
+    // Return Void
+    // Sync notifications with enabled schedules
+    func syncScheduleNotification() {
+        // Check notification already set
+        UNUserNotificationCenter.current().getPendingNotificationRequests() {
+            (notiList) in
+            for notification in notiList {
+                if (notification.identifier.contains("pet_schedule_")) {
+                    let schedule: PetSchedule? = self.scheduleList.first(where: {
+                        (schedule) in
+                        notification.identifier.split(separator: "_")[2] == String(schedule.id);
+                    });
+                    
+                    if (schedule == nil) {
+                        // Cancel pending notification if schedule not exist
+                        var identifierList: [String] = [];
+                        identifierList.append(notification.identifier);
+                        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: identifierList);
+                    } else if (!(schedule!.enabled)) {
+                        // Cancel pending notification if disabled schedule's notification is set
+                        var identifierList: [String] = [];
+                        identifierList.append("pet_schedule_\(schedule!.id)");
+                        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: identifierList);
+                    }
+                }
+            }
+        }
+        // Check notification not set yet
+        for schedule in scheduleList {
+            UNUserNotificationCenter.current().getPendingNotificationRequests() {
+                (notiList) in
+                if (!notiList.contains(where: {
+                    (notification) in
+                    notification.identifier == "pet_schedule_\(schedule.id)";
+                })) {
+                    // Setup notification if enabled schedule's notification is not set
+                    if (schedule.enabled) {
+                        self.setupNewNotificaton(schedule: schedule);
+                    }
+                };
+            }
+        }
+    }
+    
+    // func setupNewNotification
+    // No Param
+    // Return Void
+    // Send user notification for reminder
+    func setupNewNotificaton(schedule: PetSchedule) {
+        guard(UserDefaults.standard.bool(forKey: "notiPerm")) else {
+            self.present(UIUtil.makeSimplePopup(
+                title: "알림 권한 오류", message: "알림 권한을 허용하여야 리마인더 추가가 가능합니다", onClose: nil
+            ), animated: true);
+            return;
+        }
+        let content = UNMutableNotificationContent();
+        var petNameListStr = "";
+        schedule.petList.forEach() {
+            (pet) in
+            petNameListStr += "\(pet.name) ";
+        };
+        content.title = "집사의 노트";
+        content.subtitle = petNameListStr;
+        content.body = schedule.memo;
+        content.badge = 1;
+        content.sound = UNNotificationSound.default;
+        
+        let scheduleDate = PetScheduleUtil.convertTimeToDate(timeString: schedule.time);
+        var scheduleDateComponent = DateComponents();
+        scheduleDateComponent.hour = Calendar.current.component(.hour, from: scheduleDate);
+        scheduleDateComponent.minute = Calendar.current.component(.minute, from: scheduleDate);
+        let trigger = UNCalendarNotificationTrigger(dateMatching: scheduleDateComponent, repeats: true);
+        let request = UNNotificationRequest(identifier: "pet_schedule_\(schedule.id)", content: content, trigger: trigger);
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil);
     }
     
     // func reqHttpFetchPetSchedule
@@ -48,6 +139,7 @@ class UIMyPetScheduleVC: UIViewController {
             self.scheduleListTableView.delegate = self;
             self.scheduleListTableView.dataSource = nil;
             self.scheduleListTableView.dataSource = self;
+            self.syncScheduleNotification();
         }
     }
     
@@ -77,6 +169,7 @@ extension UIMyPetScheduleVC: UITableViewDelegate, UITableViewDataSource {
             cell.schedulePetNameLabel.text = PetScheduleUtil.convertPetName(petList: self.scheduleList[indexPath.row].petList);
             cell.scheduleEnabledSwitch.isOn = self.scheduleList[indexPath.row].enabled;
             cell.schedule = self.scheduleList[indexPath.row];
+            cell.delegate = self;
             return cell;
         }
     }
