da93f94b799ea4bb9196abfaf84d0c1bc3d54290
newcentury99 <smyun99@gmail.com>
2022-03-29 17:16:44 +0900

    Implement notification settings

--- pet_management/src/ui/setting/UISettingNotificationVC.swift
+++ pet_management/src/ui/setting/UISettingNotificationVC.swift
@@ -8,5 +8,32 @@
 import UIKit;
 
 class UISettingNotificationVC: UITableViewController {
+    @IBOutlet weak var communityAllNotificationSwitch: UISwitch!;
+    @IBOutlet weak var communityCommentNotificationSwitch: UISwitch!;
+    @IBOutlet weak var communityLikeNotificationSwitch: UISwitch!;
+    @IBOutlet weak var communityFollowNotificationSwitch: UISwitch!;
     
+    var accountDetails: Account?;
+    
+    override func viewDidLoad() {
+        if (accountDetails != nil) {
+            self.communityAllNotificationSwitch.isOn = accountDetails!.notification;
+            self.syncCommunityNotiSwitches();
+        }
+    }
+    
+    func syncCommunityNotiSwitches() {
+        self.communityCommentNotificationSwitch.isOn = self.communityAllNotificationSwitch.isOn;
+        self.communityLikeNotificationSwitch.isOn = self.communityAllNotificationSwitch.isOn;
+        self.communityFollowNotificationSwitch.isOn = self.communityAllNotificationSwitch.isOn;
+    }
+    
+    // Action Methods
+    @IBAction func communityAllNotiSwitchOnChange(_ sender: UISwitch) {
+        AccountUtil.reqHttpUpdateAccount(notification: self.communityAllNotificationSwitch.isOn, sender: self) {
+            (res) in
+            // Sync subcategory switches
+            self.syncCommunityNotiSwitches();
+        }
+    }
 }
